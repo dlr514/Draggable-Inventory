f910a7441131169295f0e942755f94d33dc947a4
dlr514 <62752815+dlr514@users.noreply.github.com>
2022-01-05 23:45:57 +0100

    Add files via upload

---
+++ Draggable Inventory.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Draggable_Inventory : MonoBehaviour
+{
+  private void Start()
+  {
+    mOriginalPosition = UIDragElement.localPosition;
+  }
+
+  public void OnBeginDrag(PointerEventData data)
+  {
+    mOriginalPanelLocalPosition = UIDragElement.localPosition;
+    RectTransformUtility.ScreenPointToLocalPointInRectangle(
+      Canvas, 
+      data.position, 
+      data.pressEventCamera, 
+      out mOriginalLocalPointerPosition);
+  }
+
+  public void OnDrag(PointerEventData data)
+  {
+    Vector2 localPointerPosition;
+    if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+      Canvas, 
+      data.position, 
+      data.pressEventCamera, 
+      out localPointerPosition))
+    {
+      Vector3 offsetToOriginal =
+        localPointerPosition - 
+        mOriginalLocalPointerPosition;
+
+      UIDragElement.localPosition = 
+        mOriginalPanelLocalPosition + 
+        offsetToOriginal;
+    }
+  }
+
+  public void OnEndDrag(PointerEventData eventData)
+  {
+    StartCoroutine(
+      Coroutine_MoveUIElement(      
+        UIDragElement,       
+        mOriginalPosition,       
+        0.5f));
+
+    RaycastHit hit;
+    Ray ray = Camera.main.ScreenPointToRay(
+      Input.mousePosition);
+
+    if (Physics.Raycast(ray, out hit, 1000.0f))
+    {
+      Vector3 worldPoint = hit.point;
+
+      //Debug.Log(worldPoint);
+      CreateObject(worldPoint);
+    }
+  }
+
+  public IEnumerator Coroutine_MoveUIElement(
+    RectTransform r, 
+    Vector2 targetPosition, 
+    float duration = 0.1f)
+  {
+    float elapsedTime = 0;
+    Vector2 startingPos = r.localPosition;
+    while (elapsedTime < duration)
+    {
+      r.localPosition =
+        Vector2.Lerp(
+          startingPos,
+          targetPosition, 
+          (elapsedTime / duration));
+      elapsedTime += Time.deltaTime;
+
+      yield return new WaitForEndOfFrame();
+    }
+    r.localPosition = targetPosition;
+  }
+
+  public void CreateObject(Vector3 position)
+  {
+    if (PrefabToInstantiate == null)
+    {
+      Debug.Log("No prefab to instantiate");
+      return;
+    }
+
+    GameObject obj = Instantiate(
+      PrefabToInstantiate, 
+      position, 
+      Quaternion.identity);
+  }
+}
